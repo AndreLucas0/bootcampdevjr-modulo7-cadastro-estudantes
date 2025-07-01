ff6a82f34e0c7984a4bc507fc1bbc46eed6270cd
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-07-01 12:38:18 -0300

    Adicionando projeto - closes #1

---
+++ students-backend/cadastro-de-estudantes/src/test/java/com/andre/cadastro_de_estudantes/CadastroDeEstudantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.cadastro_de_estudantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeEstudantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
