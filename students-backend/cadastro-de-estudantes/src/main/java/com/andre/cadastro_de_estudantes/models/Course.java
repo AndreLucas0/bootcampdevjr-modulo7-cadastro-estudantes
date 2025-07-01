479edf970d9f7ae10c5fa9c1a7e2cee4fb901a7b
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-07-01 12:54:13 -0300

    GET courses/id - closes #4

--- students-backend/cadastro-de-estudantes/src/main/java/com/andre/cadastro_de_estudantes/models/Course.java
+++ students-backend/cadastro-de-estudantes/src/main/java/com/andre/cadastro_de_estudantes/models/Course.java
@@ -1,5 +1,31 @@
 package com.andre.cadastro_de_estudantes.models;
 
 public class Course {
-    
+    private int id;
+    private String name;
+
+    public Course() {
+    }
+
+    public Course(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- students-backend/cadastro-de-estudantes/src/main/java/com/andre/cadastro_de_estudantes/resources/CourseController.java
+++ students-backend/cadastro-de-estudantes/src/main/java/com/andre/cadastro_de_estudantes/resources/CourseController.java
@@ -1,8 +1,34 @@
 package com.andre.cadastro_de_estudantes.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.andre.cadastro_de_estudantes.models.Course;
 
 @RestController
 public class CourseController {
+    private List<Course> courses = Arrays.asList(new Course(1, "Java"),
+                                                 new Course(2, "Spring"),
+                                                 new Course(3, "Angular"));
+
+    @GetMapping("courses/{id}")
+    public ResponseEntity<Course> getCourse(@PathVariable int id) {
+        
+        Course course = courses.stream()
+                               .filter(c -> c.getId() == id)
+                               .findFirst()
+                               .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found."));
+        
+        return ResponseEntity.ok(course);
+    }
+
     
 }
