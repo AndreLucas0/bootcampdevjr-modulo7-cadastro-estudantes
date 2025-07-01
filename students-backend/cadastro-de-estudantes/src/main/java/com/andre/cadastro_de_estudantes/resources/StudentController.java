a4d328035d59e4522eebea48b3778153c764b7b4
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-07-01 13:13:48 -0300

    POST students - closes #7

--- students-backend/cadastro-de-estudantes/src/main/java/com/andre/cadastro_de_estudantes/resources/StudentController.java
+++ students-backend/cadastro-de-estudantes/src/main/java/com/andre/cadastro_de_estudantes/resources/StudentController.java
@@ -1,15 +1,20 @@
 package com.andre.cadastro_de_estudantes.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.cadastro_de_estudantes.models.Student;
 
@@ -33,6 +38,19 @@ public class StudentController {
     @GetMapping("students")
     public List<Student> getStudents() {
         return students;
+    }
+
+    @PostMapping("students")
+    public ResponseEntity<Student> save (@RequestBody Student student) {
+        student.setId(students.size() + 1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                                  .path("/{id}")
+                                                  .buildAndExpand(student.getId())
+                                                  .toUri();
+                                                  
+        return ResponseEntity.created(location).body(student);
     }
 
 }
